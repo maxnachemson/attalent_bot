9bc044147abbfa266f2442d057cbff84a2d25731
maxnachemson <maxna@kth.se>
2017-04-09 23:23:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,74 +1,114 @@
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
 
-This is a sample Slack bot built with Botkit.
+const BeepBoop = require('beepboop-botkit');
 
-# EXTEND THE BOT:
+const beepboop = BeepBoop.start(controller, {
+    debug: true //you might want to enable debug for testing :)
+});
 
-  Botkit has many features for building cool and useful bots!
+var request = require('request');
 
-  Read all about it here:
+var options = {
+      url: 'https://script.googleusercontent.com/macros/echo?user_content_key=jQ2TC03qiZkWyvx07u8gzYWZiYtUzF5o8tkeG7unQdn2iQlye3fjmIQoIVzFfMcCSQe0qR3yPyNLONDRdDfs9Ww3xagBWEXcOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKp1E_BOymz-tQ-8TxUtrLWLSKPKjFjBmpg0EpTpFurEIVpbQEzawfLw93LnxErnS8iQ&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx'
+};
 
-    -> http://howdy.ai/botkit
+function callback(error, response, body) {
+    console.log("Downloading database...");
+    if (!error && response.statusCode == 200) {
+        info = JSON.parse(body);
+    }
+    console.log("Database ok");
+    return info;
+}
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-var STUDIO_TOKEN = process.env.BOTKIT_STUDIO_TOKEN
+//request(options, callback);
 
-var Botkit = require('botkit')
-var fs = require('fs')
-var path = require('path')
-var debug = require('debug')('botkit:main')
-var BotkitStorageBeepBoop = require('botkit-storage-beepboop')
+    
+    
 
-// Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.slackbot({
-  debug: false,
-  studio_token: STUDIO_TOKEN,
-  storage: BotkitStorageBeepBoop()
-})
+controller.hears(["Hello","Hey","Hi"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+    bot.reply(message,'Hi there!');
+    console.log("Incoming message: "+message.text);
+});
 
-controller.startTicking()
+controller.hears([/^.{0,}Thank.{0,}$/, /^.{0,}thank.{0,}$/, /^.{0,}thx.{0,}$/],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+    bot.reply(message,'No problems!');
+    console.log("Incoming message: "+message.text);
+});
 
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-require('./components/webserver/')(controller)
 
-// Register sample "skills"
-var normalizedPath = path.join(__dirname, 'skills')
-fs.readdirSync(normalizedPath).forEach(file => {
-  require(path.join(normalizedPath, file))(controller)
-})
+controller.hears([/^.{0,}job.{0,}$/], ["direct_message","direct_mention","mention","ambient"], function (bot, message) {
+  request(options, callback);
+  console.log("Incoming message: "+message.text);
+  bot.startConversation(message, function (err, convo) {
+    convo.ask('What kind of job are you looking for?', function (response, convo) {
+        console.log("Search term: "+response.text);
+        theData = info['Blad1'];
+        var results = [];
+        var toSearch = response.text;
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (STUDIO_TOKEN) {
-  controller.on('direct_message,direct_mention,mention', (bot, message) => {
-    controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
-      if (!convo) {
-        // no trigger was matched
-        // If you want your bot to respond to every message,
-        // define a 'fallback' script in Botkit Studio
-        // and uncomment the line below.
-        // controller.studio.run(bot, 'fallback', message.user, message.channel)
-      } else {
-        // set variables here that are needed for EVERY script
-        // use controller.studio.before('script') to set variables specific to a script
-        convo.setVar('current_time', new Date())
-      }
-    }).catch(err => {
-      bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err)
-      debug('Botkit Studio: ', err)
+        for(var i=0; i<theData.length; i++) {
+          for(key in theData[i]) {
+            if(theData[i][key].indexOf(toSearch)!=-1) {
+              results.push(theData[i]);
+            }
+          }
+        }
+        var resultMessage = {
+            "attachments":[]
+        };
+        //var obj = JSON.parse(jsonStr);
+        var row = 0;
+        for(var i=0; i<results.length; i++) {
+            row++;
+            console.log(row+" match");
+            newrow = '';
+            newrow = { 
+                "fallback": "jobs jobs jobs",
+                "color":"#36a64f",
+                "title": " "+results[i].title+" @"+results[i].company+" ",
+                "title_link":"http://google.com"
+        };
+        resultMessage['attachments'].push(newrow);
+        }
+
+        //console.log(resultMessage);
+
+        if (results.length > 0) {
+            if (row === 1) {
+                var ending = "";
+            } else {
+                var ending = "s";
+            }
+            convo.say("I found "+row+" opening"+ending+":");
+            convo.say(resultMessage);
+        } else {
+            console.log("0 match");
+            convo.say("Didn't find any jobs");
+            convo.ask('Could you narrow it down by choosing a category?', function (response, convo) {
+                if (response.text === 'yes') {
+                     convo.say("Thanks");
+                } else {
+                    convo.say("Ok :-(");
+                }
+                convo.next()
+            });
+
+        }
+
+      convo.next() // always call this to keep things flowing (check the readme for more info)
     })
   })
-} else {
-  console.log('~~~~~~~~~~')
-  console.log('NOTE: Botkit Studio functionality has not been enabled')
-  console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/')
-}
+})
+
+var bot = controller.spawn ({
+    token:require('./config').token
+});
+
+bot.startRTM(function(err, bot, payload) {
+    if (err) {
+        console.log(err);
+        throw new Error('Could not connect to Slack');
+    }
+});
